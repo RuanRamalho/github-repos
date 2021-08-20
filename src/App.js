08b3a8cf7ad147273f4ac6b17187ae15047f5f5f
ruan ramalho <ruanmbramalho@gmail.com>
2021-08-20 16:06:42 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [user, setUser] = useState('');
+  const [showRepos, setShowRepos] = useState(false);
+  const [errorMessageApi, setErrorMessageApi] = useState(false);
+
+  const handleInput = (event) => {
+    setUser(event.target.value);
+  };
+
+  const listRepos = (event) => {
+    event.preventDefault();
+    setUser('');
+    setErrorMessageApi(false);
+
+    api
+      .get(`/users/${user}/repos`)
+      .then((response) => setRepositories(response.data))
+      .then(setShowRepos(true))
+      .catch(() => {
+        setErrorMessageApi(true);
+      });
+  };
+
   return (
     <>
-      <h1>Olá Mundo!</h1>
+      <Container>
+        <form onSubmit={listRepos}>
+          <input type="text" value={user} onChange={handleInput} />
+          <button type="submit">Search</button>
+        </form>
+        {showRepos && (
+          <ul>
+            {repositories.map((repos) => (
+              <li key={repos.id}>{repos.name}</li>
+            ))}
+          </ul>
+        )}
+        {errorMessageApi && <h1>Error 404</h1>}
+      </Container>
     </>
   );
 }
